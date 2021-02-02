088b3df1e5e4d19a48ed97163b9f74e6ebf04706
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-02 18:13:05 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 // Define the numbers and the operation here
+let firstNumber = 2
+let secondNumber = 5
+let operation = "/"
 
+if (operation == "*"){
+    console.log(`The Operation is multiplication. *** ${firstNumber} x ${secondNumber} = ${firstNumber * secondNumber}`)
+} else if ( operation == "+"){
+    console.log(`The Operation is addition. *** ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`)
+} else if ( operation == "-"){
+    console.log(`The Operation is subtraction. *** ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`)
+} else if ( operation == "/"){
+    console.log(`The Operation is division. *** ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`)
+}
 // Write if-statements here
