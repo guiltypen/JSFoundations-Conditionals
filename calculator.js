c6771a651680072efc1a157a604124b15f252b48
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-02 19:31:41 +0300

    ternary operator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
 
+// Declaring a variable
 let firstNumber = 2
 let secondNumber = 5
-let operation = "/"
-
+let operation = "-"
+/*
+// if statment
 if (operation == "*"){
     console.log(`The Operation is multiplication. *** ${firstNumber} x ${secondNumber} = ${firstNumber * secondNumber}`)
 } else if ( operation == "+"){
@@ -12,4 +14,13 @@ if (operation == "*"){
 } else if ( operation == "/"){
     console.log(`The Operation is division. *** ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`)
 }
+*/
+let result;
 
+let check = () =>{
+
+    result = (operation == "/") ? firstNumber / secondNumber: (operation == "*") ? firstNumber * secondNumber : (operation == "-") ? firstNumber - secondNumber : (operation == "+") ? firstNumber + secondNumber : "Error"
+}
+check();
+
+console.log(result)
